5f41966fa28a15e20cfbd1fc65c22c7f9b179836
an288705 <aj0466012@gmail.com>
2022-04-10 18:36:09 -0400

    added p2.java

---
+++ p2.java
@@ -0,0 +1,9 @@
+
+//keep all readings from the past hour in 
+//an array and clear when time % 60 == 0. 
+//let a minute on mars be .5 seconds on earth.
+//your top 5 arrays are priority queues
+
+public class p2 {
+    
+}
