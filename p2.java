93cc06b825969d504392516c2d635dcc53bf8f15
an288705 <aj0466012@gmail.com>
2022-04-13 22:02:20 -0400

    p2 done

--- p2.java
+++ p2.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 //keep all readings from the past hour in 
 //an array and clear when time % 60 == 0. 
@@ -9,8 +16,8 @@
 //max heap and min heap. on each deque, remove 
 //deque val from the heaps. record the temp diff
 //of these two heaps, and update the time interval
-//to min(time of rewrite - 10,0) through 
-//min(time of rewrite,10). this guarantees interval 
+//to max(time of rewrite - 10,0) through 
+//max(time of rewrite,10). this guarantees interval 
 //of 10
 
 //since there are 4 heaps+1 queue, the heaps
@@ -22,52 +29,202 @@
 //main lock will unlock every .5 seconds then lock
 //once all operations are done. 
 
+
 public class p2 {
-    
+    public static PriorityQueue<Integer> minh = new PriorityQueue<Integer>();
+    public static PriorityQueue<Integer> maxh = new PriorityQueue<Integer>((x, y) -> Integer.compare(y, x));
+    public static PriorityQueue<Integer> minten = new PriorityQueue<Integer>();
+    public static PriorityQueue<Integer> maxten = new PriorityQueue<Integer>((x, y) -> Integer.compare(y, x));
+    public static AtomicBoolean setting = new AtomicBoolean(true);
+    public static AtomicBoolean reporting = new AtomicBoolean(false);
+    public static AtomicInteger tasks = new AtomicInteger(0);
+    public static AtomicInteger tasksDone = new AtomicInteger(0);
+    public static WaitFreeQueue queue = new WaitFreeQueue(10);
+    public static int time=0;
+    public static int prev=-1000; //init to 'null' val
+    public static int curr;
+    public static int diff = 0;
+    public static int interval=10;
+
+    public static void main(String[] args)
+    {
+        Runnable reporter = ()->{
+            while(true)
+            {
+                if(reporting.get())
+                {
+                    //report if time to report
+                    long start = System.nanoTime();
+
+                    System.out.print("Top 5 highest of the hour: ");
+                    for(int i=0;i<5;i++)
+                    {
+                        System.out.print(maxh.poll());
+                        System.out.print(" ");
+                    }
+                    System.out.print("\nTop 5 lowest of the hour: ");
+                    for(int i=0;i<5;i++)
+                    {
+                        System.out.print(minh.poll());
+                        System.out.print(" ");
+                    }
+                    System.out.print("\nThe largest temperature difference occured from "+String.valueOf(interval-10)+" to "+String.valueOf(interval)+" minutes");
+
+                    //empty data
+                    diff=0;
+                    minh = new PriorityQueue<Integer>();
+                    maxh = new PriorityQueue<Integer>((x, y) -> Integer.compare(y, x));
+                    minten = new PriorityQueue<Integer>();
+                    maxten = new PriorityQueue<Integer>((x, y) -> Integer.compare(y, x));
+
+                    long end = System.nanoTime();
+                    System.out.println("\nReport compiled in "+String.valueOf((end-start)/1000000)+" milliseconds");
+                    reporting.set(false);      
+                }
+            }
+        };
+
+        //queue can be modified but not heaps
+
+        Runnable sensor = ()->{
+            while(true)
+            {
+                if(!setting.get() && !reporting.get())
+                {
+                    while(tasksDone.get()<5)
+                    {
+                        int task = tasks.getAndIncrement();
+
+                        switch(task)
+                        {
+                            case 0:
+                                //add to q
+                                queue.enq(curr);
+                                tasksDone.getAndIncrement();
+                                break;
+                            case 1:
+                                //add to maxh
+                                maxh.add(curr);
+                                tasksDone.getAndIncrement();
+                                break;
+                            case 2:
+                                //add to minh
+                                minh.add(curr);
+                                tasksDone.getAndIncrement();
+                                break;
+                            case 3:
+                                //add and remove maxten
+                                maxten.add(curr);
+                                maxten.remove(prev);
+                                tasksDone.getAndIncrement();
+                                break;
+                            case 4:
+                                //add and remove minten
+                                minten.add(curr);
+                                minten.remove(prev);
+                                tasksDone.getAndIncrement();
+                                break;
+                            default:
+                                //wait
+                                break;
+                        }
+                    }
+
+                    if(maxten.peek()-minten.peek()>diff)
+                    {
+                        diff=maxten.peek()-minten.peek();
+                        interval = Math.max(time,60*(time/60)+10);
+                    }
+
+                    //everything is done, exit
+                    setting.set(true);
+                }   
+            }
+        };
+
+        ArrayList<Thread> threadList = new ArrayList<Thread>();
+        int N=8;
+        Random random = new Random();
+
+        threadList.add(new Thread(reporter));
+        threadList.get(0).start(); 
+
+        for(int i=1;i<N;i++)
+        {
+            threadList.add(new Thread(sensor));
+            threadList.get(i).start();    
+            
+            //for testing
+            // try{
+            //     threadList.get(i).join();  
+            // }
+            // catch(Exception e){
+            //     System.out.println(e);
+            // }
+        }
+
+        while(true)
+        {
+            //set setting=true every .1 seconds
+            try {
+                time+=1;
+                System.out.println("Time = "+String.valueOf(time)+" mins");
+                if(time%60==0) reporting.set(true);
+                setting.set(true);
+                tasks.set(0);
+                tasksDone.set(0);
+                curr = random.nextInt(171)-100;
+                prev = queue.deq();
+                setting.set(false);
+                TimeUnit.MILLISECONDS.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
 
-// [1 2 3 4 5 6 7 8 9 9]
-// heaps:
-// 1   9
-// now check
-// at time=9, (9-1)=8 diff rewritten.
-// record=min(9-10,0):min(9,10)=0:10
+class WaitFreeQueue 
+{
+    private int head;
+    private int tail; 
+    private int capacity;
+    private int[] items;
 
-// [2 3 4 5 6 7 8 9 9 100]
-// heaps:
-// 2   100
-// now check
-// at time=11, (100-2)=98 diff rewritten. 
-// record=min(11-10,0):min(11,10)=1:11
+    WaitFreeQueue(int capacity)
+    {
+        this.head=0;
+        this.tail=0;
+        this.capacity=capacity;
+        this.items = new int[capacity];
+    }
+  
+    public void enq(int x) 
+    {
+      if (this.tail-this.head == this.capacity)
+      {
+          System.out.println("enq error");
+          return;
+      }
+      this.items[tail % capacity] = x; 
+      tail++;
+      //System.out.println("added "+String.valueOf(x)+" to queue");
+    }
+    public int deq() 
+    {
+       if (this.tail == this.head || this.tail-this.head != this.capacity) return -1000;
+       int item = this.items[head % capacity]; 
+       head++;
 
-//make it go like a switch, where setting=true is 
-//setting phase, and setting=false is crud phase
-
-
-// setting=true //main in charge of tracking time and making setting=true
-// //main in charge of setting
-// set tasks=0
-// set tasksdone=0
-// set new
-// set old
-// setting=false
-// while tasksDone.getAndIncrement<6
-//     switch tasks.getAndIncrement
-//     //each case takes place concurrently
-//         case 0
-//         adding q
-//         case 1
-//         remove
-//         case 2
-//         adding maxh
-//         remove maxh
-//         case 3
-//         add minh
-//         remove minh
-//         case 4 
-//         add max10
-//         remove max10
-//         case 5
-//         add min10
-//         remove min10
-// //now update results
+       return item;
+    }
+    public void printQueue()
+    {
+        for(int i=0;i<this.capacity;i++)
+        {
+            System.out.print(this.items[i]);
+            System.out.print(" ");
+        }
+        System.out.println("");
+    }
+}
